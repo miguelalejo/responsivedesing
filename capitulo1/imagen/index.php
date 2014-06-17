839356e53295f6f4b483f486855d20324857f7dc
Miguel Alejandro Ponce Proaño <mikasomk34@hotmail.com>
2014-06-17 08:18:55 -0500

    Se agregan imagenes para el manejo de imagenes con cookies

--- capitulo1/imagen/index.php
+++ capitulo1/imagen/index.php
@@ -3,29 +3,25 @@ $screen_w=0;
 $screen_h=0;
 $img=$_SERVER['QUERY_STRING'];
 if(file_exists($img)) {
-if(isset($_COOKIE['dimension_pantalla'])) {
-$screen=explode('x',$_COOKIE['dimension_pantalla']);
-	echo $screen[0];
-	echo $screen[1];
-if(count($screen)==2) {
-$screen_w=intval($screen[0]);
-$screen_h=intval($screen[1]);
-}
-}
-echo "w".$screen_w;
-echo "h".$screen_h;
-if($screen_w>0) {
-$theExt=pathinfo($img,PATHINFO_EXTENSION);
-if($screen_w>=1024) {
-$output=substr_replace($img,'-med',-strlen($theExt)-1,0);
-} else if($screen_w<=800) {
-$output=substr_replace($img,'-low',-strlen($theExt)-1,0);
-echo $output;
-}
-if(isset($output)&&file_exists($output)) {
-$img=$output;
-}
-}
-//readfile($img);
+	if(isset($_COOKIE['dimension_pantalla'])) {
+		$screen=explode('x',$_COOKIE['dimension_pantalla']);
+		if(count($screen)==2) {
+			$screen_w=intval($screen[0]);
+			$screen_h=intval($screen[1]);
+		}
+	}
+	
+	if($screen_w>0) {
+		$theExt=pathinfo($img,PATHINFO_EXTENSION);
+		if($screen_w>=1024) {
+			$output=substr_replace($img,'-med',-strlen($theExt)-1,0);
+		} else if($screen_w<=800) {
+			$output=substr_replace($img,'-low',-strlen($theExt)-1,0);	
+		}
+		if(isset($output)&&file_exists($output)) {
+			$img=$output;
+		}
+	}
+	readfile($img);
 }
 ?>
